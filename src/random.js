65737e61bef6f13d2e1a801f9d8f57d5479ef20f
Evgenia Polozova <eugpoloz@gmail.com>
2018-05-12 12:23:16 +0300

    refactor our generator to get the best out of it

--- src/random.js
+++ src/random.js
@@ -1,38 +1,23 @@
 // @flow
 "use strict";
 
-// random number generator
-// cause I don't need anything too sophisticated
-function getIdx(array: Array<*>) {
-  const { length } = array;
-  return Math.floor(Math.random() * length);
-}
-
 type QuoteProps = {
   quotes: Array<{
     quote: string,
     author: string
   }>,
-  joke: string
+  joke: {
+    url: string,
+    text?: string
+  }
 };
 
-function pickAQuote({ quotes, joke }: QuoteProps) {
-  const quoteContainer = document.querySelector(
-    ".holyheader_quote .quote-content"
-  );
-
-  const quoteId = getIdx(quotes);
-  const currentQuote = quotes[quoteId];
-
-  if (quoteContainer instanceof HTMLElement && currentQuote !== undefined) {
-    const { quote, author } = currentQuote;
-    const quoteHTML = `${quote} <em>(c) ${author}</em> <a href="${joke}">(Смотреть котиков?)</a>`;
-
-    quoteContainer.innerHTML = quoteHTML;
-
-    return currentQuote;
-  }
-}
+type RandomProps = {
+  board?: string, // optional forum link
+  howMany?: number,
+  group_id: Array<number>,
+  filteredUsers?: Array<string>
+};
 
 type User = {
   user_id: string,
@@ -45,76 +30,108 @@ type PortraitsProps = {
   users: Array<User>
 };
 
-function createPortraits({ board = "", users }: PortraitsProps) {
-  const portraitNodeList = document.querySelectorAll(
-    ".portrait.portrait--character"
-  );
-
-  return users.forEach(({ username, user_id, avatar }, idx) => {
-    const html = `<a href="${board}/profile.php?id=${user_id}"><img src="${board}${avatar}" title="${username}" /></a>`;
-
-    if (portraitNodeList && portraitNodeList[idx]) {
-      portraitNodeList[idx].innerHTML = html;
-    }
-  });
-}
-
-type RandomProps = {
-  board?: string, // optional forum link
-  howMany?: number,
-  group_id: Array<number>,
-  filteredUsers?: Array<string>
+type HeaderProps = {
+  quotesAndJokes: QuoteProps,
+  portraits: RandomProps
 };
 
-function randomUserGenerator({
-  board = "",
-  filteredUsers = [],
-  group_id = [1, 2],
-  howMany = 3
-}: RandomProps) {
-  const group_ids = group_id.join(",");
+function workHeaderMagic({ quotesAndJokes, portraits }) {
+  // random number generator
+  // cause I don't need anything too sophisticated
+  function getIdx(array: Array<*>) {
+    const { length } = array;
+    return Math.floor(Math.random() * length);
+  }
 
-  fetch(
-    `${board}/api.php?method=users.get&fields=user_id,username,avatar&limit=100&group_id=${group_ids}`
-  )
-    .then(response => response.json())
-    .then(({ response }) => {
-      let { users } = response;
-      if (filteredUsers.length > 0) {
-        const userSet = new Set(filteredUsers);
+  function pickAQuote(quotesAndJokes: QuoteProps) {
+    const { quotes, joke } = quotesAndJokes;
+    const quoteContainer = document.querySelector(
+      ".holyheader_quote .quote-content"
+    );
 
-        users = users.filter(({ username }) => !userSet.has(username));
+    const quoteId = getIdx(quotes);
+    const currentQuote = quotes[quoteId];
+
+    if (quoteContainer instanceof HTMLElement && currentQuote !== undefined) {
+      const { quote, author } = currentQuote;
+      const { url, text = "Читать дальше" } = joke;
+      const quoteHTML = `${quote} <em>(c) ${author}</em> <a href="${url}">(${text})</a>`;
+
+      quoteContainer.innerHTML = quoteHTML;
+
+      return currentQuote;
+    }
+  }
+
+  function createPortraits({ board = "", users }: PortraitsProps) {
+    const portraitNodeList = document.querySelectorAll(
+      ".portrait.portrait--character"
+    );
+
+    return users.forEach(({ username, user_id, avatar }, idx) => {
+      const html = `<a href="${board}/profile.php?id=${user_id}"><img src="${board}${avatar}" title="${username}" /></a>`;
+
+      if (portraitNodeList && portraitNodeList[idx]) {
+        portraitNodeList[idx].innerHTML = html;
       }
+    });
+  }
 
-      // let set up our Set
-      const indexes = new Set([]);
+  function randomUserGenerator(pickPortraits: RandomProps) {
+    const {
+      board = "",
+      filteredUsers = [],
+      group_id = [1, 2],
+      howMany = 3
+    } = pickPortraits;
+    const group_ids = group_id.join(",");
 
-      // we need to generate as many numbers as our config says,
-      // and I suck at recursion, so we loop
-      for (let i = 0; i < howMany; i++) {
-        let number = getIdx(users);
+    // initialize callback hell, eh
+    fetch(
+      `${board}/api.php?method=users.get&fields=user_id,username,avatar&limit=100&group_id=${group_ids}`
+    )
+      .then(response => response.json())
+      .then(({ response }) => {
+        let { users } = response;
+        if (filteredUsers.length > 0) {
+          const userSet = new Set(filteredUsers);
 
-        // ... and loop
-        while (indexes.has(number)) {
-          number = getIdx(users);
+          users = users.filter(({ username }) => !userSet.has(username));
         }
 
-        // and add our number finally
-        indexes.add(number);
-      }
+        // let set up our Set
+        const indexes = new Set([]);
 
-      // and then we filter our initial user array to get our desired results
-      const pickedUsers = users.filter((user, idx) => indexes.has(idx));
+        // we need to generate as many numbers as our config says,
+        // and I suck at recursion, so we loop
+        for (let i = 0; i < howMany; i++) {
+          let number = getIdx(users);
 
-      // ... initiate our custom func to put portraits in their place
-      createPortraits({
-        board,
-        users: pickedUsers
-      });
+          // ... and loop
+          while (indexes.has(number)) {
+            number = getIdx(users);
+          }
 
-      return pickedUsers;
-    })
-    .catch(error => console.error(error));
+          // and add our number finally
+          indexes.add(number);
+        }
+
+        // and then we filter our initial user array to get our desired results
+        const pickedUsers = users.filter((user, idx) => indexes.has(idx));
+
+        // ... initiate our custom func to put portraits in their place
+        createPortraits({
+          board,
+          users: pickedUsers
+        });
+
+        return pickedUsers;
+      })
+      .catch(error => console.error(error));
+  }
+
+  pickAQuote(quotesAndJokes);
+  randomUserGenerator(portraits);
 }
 
 // randomUserGenerator({

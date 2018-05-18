c60fcd26cdcb237954277135a2d9c882a86f64af
Evgenia Polozova <eugpoloz@gmail.com>
2018-05-18 23:37:43 +0300

    move smilies to hsf; add production mode to webpack

--- src/hsf.js
+++ src/hsf.js
@@ -3,6 +3,7 @@
 import quotePicker from "./modules/quotePicker";
 import currentUsers from "./modules/randomUserGenerator";
 import neededCharacters from "./modules/randomSearchGenerator";
+import smilies from "./modules/smilies";
 
 import type { QuoteProps, RandomUserProps } from "./commonTypes";
 
@@ -16,7 +17,7 @@ export function workHeaderMagic({ quotesAndJokes, portraits }: HeaderProps) {
   currentUsers(portraits);
 }
 
-export { neededCharacters };
+export { neededCharacters, smilies };
 
 // randomUserGenerator({
 //   howMany: 3,
